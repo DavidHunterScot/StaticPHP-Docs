7aa0db9c523fcfb1a48418de012f7418b594085c
DavidHunterScot <david.hunter@echoverse.cc>
2023-08-10 22:26:06 +0100

    Initial Commit with Getting Started page

---
+++ src/_includes/base.php
@@ -0,0 +1,30 @@
+---
+content_placeholder: {{ content }}
+---
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>--- metadata.title --- - StaticPHP Docs</title>
+        
+        <link rel="stylesheet" type="text/css" href="https://webfonts.staticly.ict.rocks/poppins/poppins.css">
+        <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="sidebar">
+                <h1>StaticPHP Docs</h1>
+
+                <nav>
+                    <a href="/getting-started"<?php if( isset( $metadata['current_nav_item'] ) && $metadata['current_nav_item'] == "getting-started" ) echo ' class="current"'; ?>>Getting Started</a>
+                </nav>
+            </div>
+
+            <div class="content">
+                {{ content }}
+            </div>
+        </div>
+    </body>
+</html>
